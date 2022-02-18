9609e8486d5fe7798e6f55e3e003113670b83acb
codeonix <72149197+codeonix@users.noreply.github.com>
2022-02-18 11:46:11 +0530

    Add files via upload

---
+++ Factors.java
@@ -0,0 +1,31 @@
+/***************************************************************************************
+******************************* Factors of a Number ************************************
+
+Problem : Write a program to print all the factors of a number other than 1 and the
+ number itself.
+
+Algorithm :
+1.Take a number From User.
+2.Take a loop from 2 to n/2 
+3.if( n % i  ==0 ) print i.
+
+*****************************************************************************************
+-----------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------
+*****************************************************************************************/
+
+import java.util.Scanner;
+public class Factors{
+  public static void factors(int n){
+   for(int i=2;i<=n/2;i++){
+   if(n % i ==0)
+   System.out.print(i+ " ");
+  }
+}
+//Driver Code :
+ public static  void main(String ssr[]){
+   Scanner s= new Scanner(System.in);
+  int n = s.nextInt();
+  factors(n);
+  }
+}
