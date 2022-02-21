55d701e5ce0fcf2c5468a25bc29bced9ff4e411f
codeonix <72149197+codeonix@users.noreply.github.com>
2022-02-21 19:04:05 +0530

    35 . Count Zeros in a Number : Coding Ninja
    Recursively

---
+++ CountZeroRec.java
@@ -0,0 +1,32 @@
+/*************************************************************************
+ * *********************  ###Count Zero Recursively **********************
+ *************************************************************************
+ Problem :Given an integer N, count and return the number of zeros that are 
+ present in the given integer using recursion.
+ */
+
+package recursion;
+import java.util.Scanner;
+
+public class CountZeroRec {
+	public static int countZerosRec(int n){
+	
+		if(n/10 == 0 && n!=0 )
+			return 0;
+		else if(n==0)
+		return 1;
+		int lastDigit = n%10;
+		n = n/10;
+		if(lastDigit == 0)
+			return  countZerosRec(n)+1;
+		else
+			return countZerosRec(n);
+			
+	}
+	//Driver code
+	public static void main(String[] args) {
+		Scanner s= new Scanner(System.in);
+		int n = s.nextInt();
+		System.out.println(countZerosRec(n));
+	}
+}
