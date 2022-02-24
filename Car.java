0a81c54b8becda12d7b8fad58d8efb26dc6c006e
codeonix <72149197+codeonix@users.noreply.github.com>
2022-02-24 11:50:36 +0530

    46 . Inheritance -OOPS : Coding Ninja

--- Car.java
+++ Car.java
@@ -1,8 +1,16 @@
-package methodoverriding;
+package inheritance;
 
-class Car extends Vehicle{
-    void print()
-    {
-        System.out.println("car");
-    }
+public class Car extends Vehicle {
+  
+   int numDoors;
 }
+
+/****************************************
+****************************************
+public access-specifier : Project Friendly
+private  access-specifier : class Friendly
+Default access specifier : package friendly 
+protected access specifier : package friendly as well as project friendly within the derived class in another package.
+
+******************************************
+******************************************/
--- Vehicle.java
+++ Vehicle.java
@@ -1,11 +1,20 @@
-package methodoverriding;
+package inheritance;
 
-class Vehicle{
-    String colour;
-    private int number;
-    void print()
-    {
-        System.out.println("vehicle");
-    }
+public class Vehicle {
+ private String color;
+ protected  int maxSpeed;
+ protected  int price;
+ 
+ public String getColor() {
+	 return this.color;
+ }
+ public void setColor(String color) {
+	 this.color = color;
+ }
+ public void print() {
+	 System.out.println("Color is :"+color);
+	 System.out.println("Max speed is :"+maxSpeed );
+	 System.out.println("Price is "+price);
+ }
+ 
 }
-
